3c9b0a7752ec895b412189b6480aff0a9d831358
Keloran <keloran@develbox.info>
2024-02-07 23:27:33 +0000

    added adding harbor secret to kubernetes

--- apps/harbor.go
+++ apps/harbor.go
@@ -1,145 +1,171 @@
 package apps
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/widget"
-	xwidget "fyne.io/x/fyne/widget"
-	"github.com/bugfixes/go-bugfixes/logs"
-	"github.com/chewedfeed/automated/internal/harbor"
-	"time"
+  "fmt"
+  "time"
+
+  "fyne.io/fyne/v2"
+  "fyne.io/fyne/v2/container"
+  "fyne.io/fyne/v2/dialog"
+  "fyne.io/fyne/v2/widget"
+  xwidget "fyne.io/x/fyne/widget"
+  "github.com/bugfixes/go-bugfixes/logs"
+  "github.com/chewedfeed/automated/internal/harbor"
+  "github.com/chewedfeed/automated/internal/kubernetes"
 )
 
 func getHarborProjects() ([]string, error) {
-	h, err := harbor.NewHarbor()
-	if err != nil {
-		return nil, err
-	}
-	projects, err := h.GetProjects()
-	if err != nil {
-		return nil, err
-	}
+  h, err := harbor.NewHarbor()
+  if err != nil {
+    return nil, err
+  }
+  projects, err := h.GetProjects()
+  if err != nil {
+    return nil, err
+  }
 
-	return projects, nil
+  return projects, nil
 }
 
 func HarborRegistrySecretView(w fyne.Window) fyne.CanvasObject {
-	robotName := widget.NewEntry()
-	robotName.SetPlaceHolder("Robot Name")
+  robotName := widget.NewEntry()
+  robotName.SetPlaceHolder("Robot Name")
 
-	harborProjects, err := getHarborProjects()
-	if err != nil {
-		fyne.LogError("Failed to get harbor projects", err)
-	}
-	projectSelect := widget.NewSelect(harborProjects, nil)
-	robotSecret := widget.NewEntry()
-	robotSecret.SetPlaceHolder("Robot Secret")
-	var expireDate time.Time
+  harborProjects, err := getHarborProjects()
+  if err != nil {
+    fyne.LogError("Failed to get harbor projects", err)
+  }
+  harborProjectSelect := widget.NewSelect(harborProjects, nil)
+  robotSecret := widget.NewEntry()
+  robotSecret.SetPlaceHolder("Robot Secret")
+  var expireDate time.Time
 
-	robotCreateDetails := harbor.RobotDetails{}
-	createRobot := widget.NewButton("Create Robot", func() {
-		robotType := widget.NewRadioGroup([]string{"System", "Project"}, nil)
-		l := widget.NewLabel("None Expiring")
-		l.Alignment = fyne.TextAlignLeading
-		i := widget.NewLabel("Select Date")
-		i.Alignment = fyne.TextAlignLeading
-		datePicker := xwidget.NewCalendar(time.Now(), func(date time.Time) {
-			l.SetText(date.Format("2006-01-02"))
-			expireDate = date
-		})
+  k, err := kubernetes.NewKubernetes()
+  if err != nil {
+    dialog.ShowError(logs.Local().Errorf("failed to get kubernetes: %+v", err), w)
+  }
 
-		formItems := []*widget.FormItem{
-			{Text: "Robot Type", Widget: robotType},
-			{Text: "Selected Date", Widget: l},
-			{Text: "Expire Date", Widget: datePicker},
-		}
+  kns, err := k.GetNamespaces()
+  if err != nil {
+    dialog.ShowError(logs.Local().Errorf("failed to get kubernetes namespaces: %+v", err), w)
+  }
+  kubernetesNamespaceSelect := widget.NewSelect(kns, nil)
 
-		robotSecret.Disabled()
-		dialog.ShowForm("Create Robot", "Create Robot", "Cancel", formItems, func(submitted bool) {
-			if !submitted {
-				return
-			}
-			systemRobot := false
-			if robotType.Selected == "System" {
-				systemRobot = true
-			}
+  robotCreateDetails := harbor.RobotDetails{}
+  createRobot := widget.NewButton("Create Robot", func() {
+    robotType := widget.NewRadioGroup([]string{"System", "Project"}, nil)
+    l := widget.NewLabel("None Expiring")
+    l.Alignment = fyne.TextAlignLeading
+    i := widget.NewLabel("Select Date")
+    i.Alignment = fyne.TextAlignLeading
+    datePicker := xwidget.NewCalendar(time.Now(), func(date time.Time) {
+      l.SetText(date.Format("2006-01-02"))
+      expireDate = date
+    })
 
-			if robotName.Text == "" {
-				dialog.ShowError(logs.Local().Errorf("Robot Name Needs to be input"), w)
-				return
-			}
-			if projectSelect.Selected == "" {
-				dialog.ShowError(logs.Local().Errorf("Project Needs to be selected"), w)
-				return
-			}
+    formItems := []*widget.FormItem{
+      {Text: "Robot Type", Widget: robotType},
+      {Text: "Selected Date", Widget: l},
+      {Text: "Expire Date", Widget: datePicker},
+    }
 
-			if robotType.Selected == "" {
-				dialog.ShowError(logs.Local().Errorf("Robot Type Needs to be selected"), w)
-				return
-			}
+    robotSecret.Disabled()
+    dialog.ShowForm("Create Robot", "Create Robot", "Cancel", formItems, func(submitted bool) {
+      if !submitted {
+        return
+      }
+      systemRobot := false
+      if robotType.Selected == "System" {
+        systemRobot = true
+      }
 
-			h, err := harbor.NewHarbor()
-			if err != nil {
-				fyne.LogError("Failed to create harbor client", err)
-			}
-			r, err := h.CreateRobot(robotName.Text, projectSelect.Selected, systemRobot, expireDate)
-			if err != nil {
-				fyne.LogError("Failed to create robot", err)
-			}
-			robotCreateDetails = *r
-		}, w)
-	})
+      if robotName.Text == "" {
+        dialog.ShowError(logs.Local().Errorf("Robot Name Needs to be input"), w)
+        return
+      }
+      if harborProjectSelect.Selected == "" {
+        dialog.ShowError(logs.Local().Errorf("Project Needs to be selected"), w)
+        return
+      }
 
-	form := &widget.Form{
-		Items: []*widget.FormItem{
-			{Text: "Robot Name", Widget: robotName},
-			{Text: "Project", Widget: projectSelect},
-			{Text: "Create Robot", Widget: createRobot},
-			{Text: "Robot Secret", Widget: robotSecret},
-		},
-		OnSubmit: func() {
-			robotValid := false
-			if robotCreateDetails.Name != "" {
-				robotValid = true
-			}
-			robotDetails := harbor.RobotDetails{}
+      if robotType.Selected == "" {
+        dialog.ShowError(logs.Local().Errorf("Robot Type Needs to be selected"), w)
+        return
+      }
 
-			if projectSelect.Selected == "" {
-				fyne.CurrentApp().SendNotification(&fyne.Notification{
-					Title:   "Error",
-					Content: "Please select a project",
-				})
-			}
+      h, err := harbor.NewHarbor()
+      if err != nil {
+        fyne.LogError("Failed to create harbor client", err)
+      }
+      r, err := h.CreateRobot(robotName.Text, harborProjectSelect.Selected, systemRobot, expireDate)
+      if err != nil {
+        fyne.LogError("Failed to create robot", err)
+      }
+      robotCreateDetails = *r
+      robotSecret.Text = r.Secret
+    }, w)
+  })
 
-			h, err := harbor.NewHarbor()
-			if err != nil {
-				fyne.LogError("Failed to create harbor client", err)
-			}
+  form := &widget.Form{
+    Items: []*widget.FormItem{
+      {Text: "Robot Name", Widget: robotName},
+      {Text: "Harbor Project", Widget: harborProjectSelect},
+      {Text: "Create Robot", Widget: createRobot},
+      {Text: "Robot Secret", Widget: robotSecret},
+      {Text: "Kubernetes Namespace", Widget: kubernetesNamespaceSelect},
+    },
+    OnSubmit: func() {
+      robotValid := false
+      if robotCreateDetails.Name != "" {
+        robotValid = true
+      }
+      robotDetails := harbor.RobotDetails{}
 
-			valid, err := h.ValidateRobot(robotName.Text, robotSecret.Text)
-			if err != nil {
-				fyne.LogError("Failed to validate robot", err)
-			}
-			if !valid {
-				fyne.LogError("Invalid robot", err)
-			}
-			robotValid = valid
-			robotDetails = harbor.RobotDetails{
-				Name:   robotName.Text,
-				Secret: robotSecret.Text,
-			}
+      if harborProjectSelect.Selected == "" {
+        fyne.CurrentApp().SendNotification(&fyne.Notification{
+          Title:   "Error",
+          Content: "Please select a harbor project",
+        })
+      }
 
-			if robotValid {
-				logs.Local().Infof("robot details: %+v", robotDetails)
-			}
+      h, err := harbor.NewHarbor()
+      if err != nil {
+        fyne.LogError("Failed to create harbor client", err)
+      }
 
-			// logs.Local().Infof("Submitted form with robot name: %s, project: %s, create: %b", robotName.Text, projectSelect.Selected, createRobot.Checked)
-		},
-		OnCancel: func() {
-			logs.Local().Infof("Cancelled form")
-		},
-	}
+      valid, err := h.ValidateRobot(robotName.Text, robotSecret.Text)
+      if err != nil {
+        fyne.LogError("Failed to validate robot", err)
+      }
+      if !valid {
+        fyne.LogError("Invalid robot", err)
+      }
+      robotValid = valid
+      robotDetails = harbor.RobotDetails{
+        Name:   robotName.Text,
+        Secret: robotSecret.Text,
+      }
 
-	return container.NewVBox(form)
+      if kubernetesNamespaceSelect.Selected == "" {
+        fyne.CurrentApp().SendNotification(&fyne.Notification{
+          Title:   "Error",
+          Content: "Please select a kubernetes n",
+        })
+      }
+
+      rName := fmt.Sprintf("robot$%s+%s", harborProjectSelect.Selected, robotName.Text)
+      k.CreateDockerSecret("https://containers.chewed-k8s.net", rName, robotSecret.Text, "containers@chewed-k8s.net", kubernetesNamespaceSelect.Selected)
+
+      if robotValid {
+        logs.Local().Infof("robot details: %+v", robotDetails)
+      }
+
+      // logs.Local().Infof("Submitted form with robot name: %s, project: %s, create: %b", robotName.Text, projectSelect.Selected, createRobot.Checked)
+    },
+    OnCancel: func() {
+      logs.Local().Infof("Cancelled form")
+    },
+  }
+
+  return container.NewVBox(form)
 }
--- internal/kubernetes/kubernetes.go
+++ internal/kubernetes/kubernetes.go
@@ -1,61 +1,95 @@
 package kubernetes
 
 import (
-	"context"
-	"fmt"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+  "context"
+  "encoding/json"
+  "fmt"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+  v1 "k8s.io/api/core/v1"
+  "k8s.io/client-go/kubernetes"
+  "k8s.io/client-go/tools/clientcmd"
+  "k8s.io/client-go/util/homedir"
+
+  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Kubernetes struct {
-	clientset *kubernetes.Clientset
+  clientset *kubernetes.Clientset
 }
 
 func NewKubernetes() (*Kubernetes, error) {
-	cfg, err := clientcmd.BuildConfigFromFlags("", fmt.Sprintf("%s/.kube/config", homedir.HomeDir()))
-	if err != nil {
-		return nil, err
-	}
+  cfg, err := clientcmd.BuildConfigFromFlags("", fmt.Sprintf("%s/.kube/config", homedir.HomeDir()))
+  if err != nil {
+    return nil, err
+  }
 
-	clientset, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
+  clientset, err := kubernetes.NewForConfig(cfg)
+  if err != nil {
+    return nil, err
+  }
 
-	return &Kubernetes{
-		clientset: clientset,
-	}, nil
+  return &Kubernetes{
+    clientset: clientset,
+  }, nil
 }
 
 func (k *Kubernetes) GetNamespaces() ([]string, error) {
-	ns, err := k.clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	names := make([]string, 0)
-	for _, n := range ns.Items {
-		names = append(names, n.Name)
-	}
+  ns, err := k.clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+  if err != nil {
+    return nil, err
+  }
+  names := make([]string, 0)
+  for _, n := range ns.Items {
+    names = append(names, n.Name)
+  }
 
-	return names, nil
+  return names, nil
 }
 
 func (k *Kubernetes) CreateSecret(name, namespace string, data map[string]string) error {
-	secretData := make(map[string][]byte)
-	for k, v := range data {
-		secretData[k] = []byte(v)
-	}
+  secretData := make(map[string][]byte)
+  for k, v := range data {
+    secretData[k] = []byte(v)
+  }
 
-	_, err := k.clientset.CoreV1().Secrets(namespace).Create(context.Background(), &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Type: v1.SecretTypeOpaque,
-		Data: secretData,
-	}, metav1.CreateOptions{})
-	return err
+  _, err := k.clientset.CoreV1().Secrets(namespace).Create(context.Background(), &v1.Secret{
+    ObjectMeta: metav1.ObjectMeta{
+      Name: name,
+    },
+    Type: v1.SecretTypeOpaque,
+    Data: secretData,
+  }, metav1.CreateOptions{})
+  return err
+}
+
+func (k *Kubernetes) CreateDockerSecret(server, username, password, email, namespace string) error {
+  type dockerAuth struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+    Email    string `json:"email"`
+  }
+
+  type dockerAuths struct {
+    Auths map[string]dockerAuth `json:"auths"`
+  }
+
+  auth := dockerAuths{
+    Auths: map[string]dockerAuth{
+      server: {
+        Username: username,
+        Password: password,
+        Email:    email,
+      },
+    },
+  }
+
+  secretData, err := json.Marshal(auth)
+  if err != nil {
+    return err
+  }
+
+  secret := make(map[string]string)
+  secret[".dockerconfigjson"] = fmt.Sprintf("%s", secretData)
+
+  return k.CreateSecret("docker-registry-secret", namespace, secret)
 }
